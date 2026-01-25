89458850db1610ae01a7dca2622c091fce1a217e
sujal-shrestha <sujalwork69@gmail.com>
2026-01-26 00:43:45 +0545

    feat: add upload middleware

---
+++ src/middlewares/upload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) cb(null, true);
+  else cb(new Error("Only images allowed"), false);
+};
+
+module.exports = multer({ storage, fileFilter });
