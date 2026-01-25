8ed2dba520a47f5da0001275bc4d7fe0bf0e0889
sujal-shrestha <sujalwork69@gmail.com>
2026-01-26 00:43:45 +0545

    feat: add database config

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+module.exports = async function connectDB() {
+  const uri = process.env.MONGO_URI;
+  if (!uri) throw new Error("MONGO_URI missing in .env");
+
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+  console.log("✅ MongoDB connected");
+};
