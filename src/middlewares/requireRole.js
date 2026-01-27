6468f668ac539d9eab1107753943ffab41d808bb
sujal-shrestha <sujalwork69@gmail.com>
2026-01-27 16:46:18 +0545

    feat add role middleware

---
+++ src/middlewares/requireRole.js
@@ -0,0 +1,10 @@
+exports.requireRole = (role) => {
+  return (req, res, next) => {
+    if (!req.user || req.user.role !== role) {
+      return res.status(403).json({
+        message: "Access denied. Insufficient permissions.",
+      });
+    }
+    next();
+  };
+};
