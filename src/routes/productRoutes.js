e0f2facd2fbbfe7002862bcb7d1101dd42e3de4b
sujal-shrestha <sujalwork69@gmail.com>
2026-01-27 16:46:18 +0545

    feat update product routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const product = require("../controllers/productController");
 const { requireAuth, requireAdmin } = require("../middlewares/auth");
 
-// Public
+// PUBLIC
 router.get("/", product.listProducts);
 
-// Admin
+// ADMIN
+router.get("/admin", requireAuth, requireAdmin, product.adminListProducts);
 router.post("/", requireAuth, requireAdmin, product.createProduct);
+router.patch("/:id/toggle", requireAuth, requireAdmin, product.toggleProduct);
+router.delete("/:id", requireAuth, requireAdmin, product.deleteProduct);
 
 module.exports = router;
